e8250f56fe37ae8553fda5abc5f02fe0c03f613f
Oleg <33009369+tpov@users.noreply.github.com>
2021-10-26 17:08:58 +0300

    GeoQuiz1.0

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -1,21 +1,24 @@
 package com.bignerdranch.android.geomain
 
+//import androidx.lifecycle.ViewModelProviders
+//import com.bignerdranch.android.geoquiz.QuizViewModel
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-//import androidx.lifecycle.ViewModelProviders
-//import com.bignerdranch.android.geoquiz.QuizViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+
+//import androidx.activity.result.contract.ActivityResultContracts
+//import androidx.appcompat.app.AppCompatActivity
+
 
 private const val TAG = "MainActivity"
 private const val KEY_INDEX = "index"
+private const val REQUEST_CODE_CHEAT = 0
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,22 +26,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var falseButton: Button
     private lateinit var nextButton: Button
     private lateinit var prefButton: Button
+    private lateinit var cheatButton: Button
     private lateinit var questionTextView: TextView
 
+    private var currentIndex = 0
+    private var isCheater = false
     private var constCurrentIndex = 0
     private var points = 0
     private var persentPoints = 0
-
     private var codeMap = "111111"
     private var charMap = ""
     private var i = 0
     private var j = 0
 
-    //private const val TAG = "QuizViewModel"
-    //private val quizViewModel: QuizViewModel by lazy {
-      //  ViewModelProviders.of(this).get(QuizViewModel::class.java)
-    //}
-
+    /*
+    private const val TAG = "QuizViewModel"
+    private val quizViewModel: QuizViewModel by lazy {
+    ViewModelProviders.of(this).get(QuizViewModel::class.java)
+    }
+    */
 
     private var mapAnswer: MutableMap<Int, Boolean> = mutableMapOf(
         0 to true,
@@ -50,9 +56,6 @@ class MainActivity : AppCompatActivity() {
     )
 
     //private const val TAG = "QuizViewModel"
-
-        private var currentIndex = 0
-
         private val questionBank = listOf(
             Question(R.string.question_australia, true),
             Question(R.string.question_oceans, true),
@@ -61,7 +64,7 @@ class MainActivity : AppCompatActivity() {
             Question(R.string.question_americas, true),
             Question(R.string.question_asia, true)
         )
-
+    //@SuppressLint("RestrictedAPI")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //Log.d(TAG, "onCreate(Bundle?) called")
@@ -75,9 +78,8 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
         nextButton = findViewById(R.id.next_button)
         prefButton = findViewById(R.id.pref_button)
+        cheatButton = findViewById(R.id.cheat_button)
         questionTextView = findViewById(R.id.question_text_view)
-
-
 
         trueButton.setOnClickListener { view: View ->
             checkBlockMap()
@@ -85,7 +87,9 @@ class MainActivity : AppCompatActivity() {
             coderBlockMap()
             checkAnswer(true)
             constCurrentIndex += 1
-
+            if (constCurrentIndex == 6) {
+                result(points)
+            }
         }
         falseButton.setOnClickListener { view: View ->
             checkBlockMap()
@@ -93,13 +97,28 @@ class MainActivity : AppCompatActivity() {
             coderBlockMap()
             checkAnswer(false)
             constCurrentIndex += 1
+            if (constCurrentIndex == 6) {
+                result(points)
+            }
+        }
+        cheatButton.setOnClickListener { view ->
+            //val intent = Intent(this, CheatActivity::class.java)
+
+            val answerIsTrue = currentQuestionAnswer
+            val intent = CheatActivity.newIntent(this@MainActivity, answerIsTrue)
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                val options = ActivityOptions.makeClipRevealAnimation(view, 0, 0, view.width, view.height)
+                startActivityForResult(intent, REQUEST_CODE_CHEAT, options.toBundle())
+            } else {
+                startActivityForResult(intent, REQUEST_CODE_CHEAT)
+            }
 
         }
         nextButton.setOnClickListener {
             if (currentIndex == 5) {
                 val toastNull = Toast.makeText(this, R.string.null_toast, Toast.LENGTH_SHORT)
                 toastNull.show()
-                result(points)
             } else {
                 moveToNext()
                 updateQuestion()
@@ -118,6 +137,16 @@ class MainActivity : AppCompatActivity() {
         updateQuestion()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode != Activity.RESULT_OK) {
+            return
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            isCheater = data?.getBooleanExtra(EXTRA_ANSWER_SHOW, false) ?: false
+        }
+    }
+
     fun moveToPref() {
         currentIndex = (currentIndex - 1) % questionBank.size
     }
@@ -128,6 +157,7 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         outState?.run {
             putInt("currentIndex", currentIndex)
+            putInt("constCurrentIndex", constCurrentIndex)
             putString("codeMap", codeMap)
             putInt("points", points)
             putInt("persentPoints", persentPoints)
@@ -137,6 +167,7 @@ class MainActivity : AppCompatActivity() {
     override fun onRestoreInstanceState(saveInstanceState: Bundle) {
         super.onRestoreInstanceState(saveInstanceState)
         currentIndex = saveInstanceState?.getInt("currentIndex")!!
+        constCurrentIndex = saveInstanceState?.getInt("constCurrentIndex")!!
         codeMap = saveInstanceState?.getString("codeMap")!!
         points = saveInstanceState.getInt("points")!!
         persentPoints = saveInstanceState.getInt("persentPoints")!!
@@ -178,6 +209,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateQuestion() {
+        //Log.d(TAG, "Updating question text", Exception())
         val questionTextResId = currentQuestionText
         questionTextView.setText(questionTextResId)
     }
@@ -189,13 +221,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun checkAnswer(userAnswer: Boolean) {
         val correctAnswer = currentQuestionAnswer
-        val messageResId = if(userAnswer == correctAnswer) {
-            points += 1
-            R.string.correct_toast
-        } else {
-            R.string.incorrect_toast
+        val messageResId = when {
+            isCheater -> R.string.judgment_toast
+            userAnswer == correctAnswer -> {
+                points += 1
+                R.string.correct_toast
+
+            }
+            else -> R.string.incorrect_toast
         }
-        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+        val toastCheckAnswer = Toast.makeText(this, messageResId, Toast.LENGTH_SHORT)
+        toastCheckAnswer.show()
     }
     private fun result(points: Int) {
         persentPoints = points*100/6
